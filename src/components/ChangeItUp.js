fcdb86eb812993c668234fb6ee4ebc53b802931a
Joshua <joshua.gatune@student.moringaschool.com>
2023-07-20 10:59:10 +0300

    inittial commit

--- src/components/ChangeItUp.js
+++ src/components/ChangeItUp.js
@@ -1,5 +1,28 @@
 import React from "react";
 
+// function ChangeItUp() {
+//   function handleChange(event) {
+//     console.log(`${event.target.name}: ${event.target.value}`);
+//   }
+
+//   return (
+//     <div>
+//       <input
+//         type="text"
+//         name="search"
+//         onChange={handleChange}
+//         placeholder="Enter search term..."
+//       />
+
+//       <select name="filter" onChange={handleChange}>
+//         <option value="all">Select a filter...</option>
+//         <option value="completed">Completed</option>
+//         <option value="incomplete">Incomplete</option>
+//       </select>
+//     </div>
+//   );
+// }
+
 function ChangeItUp() {
   function handleChange(event) {
     console.log(`${event.target.name}: ${event.target.value}`);
@@ -22,5 +45,4 @@ function ChangeItUp() {
     </div>
   );
 }
-
 export default ChangeItUp;
